5f324e00004f7f6e7aab4cb7a37a8f7e06f721d8
md-zeon <zeonrahaman5870@gmail.com>
2025-11-24 02:54:40 +0600

    fix: Update header to restrict access to admin only and change branding from 'BUBT' to 'JU'

--- teacher/header.php
+++ teacher/header.php
@@ -2,7 +2,7 @@
 include_once '../dbconnection.php';
 session_start();
 
-if (!isset($_SESSION['user']) || !strcmp($_SESSION['user'], "teacher") == 0) {
+if (!isset($_SESSION['user']) || strcmp($_SESSION['user'], "admin") != 0) {
     header("Location:../index.php");
 }
 
@@ -15,7 +15,8 @@ if (!isset($_SESSION['user']) || !strcmp($_SESSION['user'], "teacher") == 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <title>Exam - Dashboard</title>
     <link rel="shortcut icon" href="assets/img/favicon.png">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&amp;display=swap">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&amp;display=swap">
     <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
@@ -28,7 +29,7 @@ if (!isset($_SESSION['user']) || !strcmp($_SESSION['user'], "teacher") == 0) {
         <div class="header">
             <div class="header-left">
                 <a href="index.php" class="logo">
-                    <img src="assets/img/logo.png" alt="Logo"> BUBT
+                    <img src="assets/img/logo.png" alt="Logo"> JU
                 </a>
                 <a href="index.php" class="logo logo-small">
                     <img src="assets/img/logo-small.png" alt="Logo" width="30" height="30">
@@ -59,20 +60,23 @@ if (!isset($_SESSION['user']) || !strcmp($_SESSION['user'], "teacher") == 0) {
                             <a href="index.php"><i class="fas fa-user-graduate"></i> <span> Dashboard</span></a>
 
                         </li>
-                        <li class="submenu" id="students" >
-                            <a href="#"><i class="fas fa-user-graduate"></i> <span> Students</span> <span class="menu-arrow"></span></a>
+                        <li class="submenu" id="students">
+                            <a href="#"><i class="fas fa-user-graduate"></i> <span> Students</span> <span
+                                    class="menu-arrow"></span></a>
                             <ul>
                                 <li><a href="studentlist.php">Student List</a></li>
                                 <li><a href="studentadd.php">Student Add</a></li>
                             </ul>
                         </li>
                         <li id="teachers">
-                            <a href="teacherlist.php"><i class="fas fa-chalkboard-teacher"></i> <span> Teachers</span></a>
+                            <a href="teacherlist.php"><i class="fas fa-chalkboard-teacher"></i> <span>
+                                    Teachers</span></a>
 
                         </li>
 
                         <li class="submenu" id="courses">
-                            <a href="#"><i class="fas fa-book-reader"></i> <span> Courses</span> <span class="menu-arrow"></span></a>
+                            <a href="#"><i class="fas fa-book-reader"></i> <span> Courses</span> <span
+                                    class="menu-arrow"></span></a>
                             <ul>
                                 <li><a href="courselist.php">Course List</a></li>
                                 <li><a href="courseadd.php">Course Add</a></li>
@@ -80,7 +84,8 @@ if (!isset($_SESSION['user']) || !strcmp($_SESSION['user'], "teacher") == 0) {
                         </li>
 
                         <li class="submenu" id="rooms">
-                            <a href="#"><i class="fas fa-hotel"></i> <span> Rooms</span> <span class="menu-arrow"></span></a>
+                            <a href="#"><i class="fas fa-hotel"></i> <span> Rooms</span> <span
+                                    class="menu-arrow"></span></a>
                             <ul>
                                 <li><a href="roomlist.php">Room List</a></li>
                                 <li><a href="roomadd.php">Room Add</a></li>
@@ -93,7 +98,8 @@ if (!isset($_SESSION['user']) || !strcmp($_SESSION['user'], "teacher") == 0) {
 
 
                         <li class="submenu" id="exams">
-                            <a href="#"><i class="fas fa-clipboard-list"></i> <span> Exam </span> <span class="menu-arrow"></span></a>
+                            <a href="#"><i class="fas fa-clipboard-list"></i> <span> Exam </span> <span
+                                    class="menu-arrow"></span></a>
                             <ul>
                                 <li><a href="examlist.php">Exam List</a></li>
                                 <li><a href="examadd.php">Exam Add</a></li>
@@ -103,12 +109,13 @@ if (!isset($_SESSION['user']) || !strcmp($_SESSION['user'], "teacher") == 0) {
                             <span>Pages</span>
                         </li>
                         <li class="submenu" id="actions">
-                            <a href="#"><i class="fas fa-shield-alt"></i> <span> Action </span> <span class="menu-arrow"></span></a>
+                            <a href="#"><i class="fas fa-shield-alt"></i> <span> Action </span> <span
+                                    class="menu-arrow"></span></a>
                             <ul>
                                 <li><a href="changepassword.php">Change Password</a></li>
                                 <li><a href="logout.php">Log Out</a></li>
                             </ul>
-                            
+
                         </li>
 
 
